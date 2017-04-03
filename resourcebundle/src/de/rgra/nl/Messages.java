05780ca21a406aec2c7970d999223ddd540aa026
rgra <rgransberger@gmx.de>
2017-04-03 13:24:45 +0200

    Simple Test with Resource Bundle not working in Module

---
+++ resourcebundle/src/de/rgra/nl/Messages.java
@@ -0,0 +1,25 @@
+package de.rgra.nl;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "de.rgra.nl.messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Messages.getString("I18n.message")); //$NON-NLS-1$
+	}
+}
---
+++ resourcebundle/src/module-info.java
@@ -0,0 +1,5 @@
+module resourcebundle {
+	
+	requires java.base;
+	
+}
